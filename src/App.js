e46d171d54879f55298a42f9d9620b51f78d9af0
Satendra Kumar <satendramca2016@gmail.com>
2022-07-20 12:32:40 +0530

    add code

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import TodoForm from "./Components/TodoForm";
 import HomePage from "./Components/HomePage";
 
 import { BrowserRouter } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <TodoProvider>
+          <NavBar/>
           <TodoForm />
           <HomePage />
         </TodoProvider>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,17 +3,16 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 function NavBar() {
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><h2>Machine Test</h2></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
-          <Nav>
+          <Nav >
             <Nav.Link href="#deets">
               <h4>All Users</h4>
             </Nav.Link>
